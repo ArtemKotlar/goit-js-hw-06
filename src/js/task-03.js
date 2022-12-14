67b26f8de1db66455db784e6dfe0e015fce0d32c
ArtemKotlar <artemkotlar93@gmail.com>
2022-12-15 01:25:09 +0200

    task-03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,14 +1,41 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const listRef = document.querySelector(".gallery");
+
+const image = images
+  .map(
+    (img) =>
+      `<li class="gallery-item"><img src="${img.url}" alt="${img.alt}" width=320 height=210></li>`
+  )
+  .join("");
+
+listRef.style = `display:flex; flex-wrap:wrap; gap:30px; list-style:none`;
+
+listRef.insertAdjacentHTML("beforeend", image);
+
+// listRef.style.display = "flex";
+// listRef.style.flexWrap = "wrap";
+// listRef.style.gap = "30px";
+// listRef.style.listStyle = "none";
+
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
